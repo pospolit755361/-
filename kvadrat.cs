be2f4c3777311acddc3896a175ff2c930bd862bd
pospolit755361 <163743087+pospolit755361@users.noreply.github.com>
2024-03-17 17:48:39 +0300

    Add files via upload

---
+++ kvadrat.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp21
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a, b, c, d;
+            a = Convert.ToDouble(Console.ReadLine());
+            b = Convert.ToDouble(Console.ReadLine());
+            c = Convert.ToDouble(Console.ReadLine());
+            d = b * b - 4 * a * c;
+            if (d > 0)
+            {
+                double x1 = (-b * (-Math.Sqrt(d))) / (2 * a);
+                double x2 = (-b * Math.Sqrt(d)) / (2 * a);
+                Console.WriteLine(x1);
+                Console.WriteLine(x2);
+            }
+            else if(d == 0){
+                double x = -b / (2 * a);
+                Console.WriteLine(x);
+            }
+            else
+            {
+                Console.WriteLine("Нет корней");
+            }
+            Console.ReadLine();
+        }
+    }
+}
